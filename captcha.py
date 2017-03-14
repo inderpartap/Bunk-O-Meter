93a39b1381b52a3430a747b5b4623fc346335bfa
Raghav Kakkar <rkakkar7@gmail.com>
2017-03-14 14:30:20 +0530

    test

--- captcha.py
+++ captcha.py
@@ -1,6 +1,6 @@
 import cv2.cv as cv
 import pytesseract
-gray = cv.LoadImage('.\gray_captcha.png', cv.CV_LOAD_IMAGE_GRAYSCALE)
+gray = cv.LoadImage('.\gray1_captcha.png', cv.CV_LOAD_IMAGE_GRAYSCALE)
 cv.Threshold(gray, gray, 231, 255, cv.CV_THRESH_BINARY)
 api = pytesseract.TessBaseAPI()
 api.Init(".","eng",tesseract.OEM_DEFAULT)
