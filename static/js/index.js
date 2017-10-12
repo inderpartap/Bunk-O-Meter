ad2b42de18ef0254341d49ef3f4dc715a8bcedf0
inderpartap <inderpartaps.cheema2014@vit.ac.in>
2017-10-13 02:07:45 +0530

    Fixed the eye movement of panda

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,7 @@ $('#password').focusout(function(){
 $(document).on( "mousemove", function( event ) {
   var dw = $(document).width() / 15;
   var dh = $(document).height() / 15;
-  var x = event.pageX/ dw;
+  var x = (event.pageX/ dw)-5;
   var y = event.pageY/ dh;
   $('.eye-ball').css({
     width : x,
